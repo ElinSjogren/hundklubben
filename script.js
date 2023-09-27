f63634dec2d3881d0be0dda88095d4413872b817
elin.sjogren <elin.rn.sjogren@gmail.com>
2023-09-27 21:14:13 +0200

    bugg i js

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 let rattLosen = "qwe123";
 let anvandarNamn = "Bella";
+
+window.onload = (event) =>{
+  
+    let namnRuta = document.getElementById('namn');
+    let losenRuta = document.getElementById('losenord');
+    const knappen = document.getElementById('loggaInKnapp');
+
+    knappen.addEventListener("click", function(event){
+      event.preventDefault();
+      let inLosen = losenRuta.innerHTML;
+      let inNamn = namnRuta.innerHTML;
+      console.log(inLosen + inNamn); 
+      localStorage.setItem("losen", losenRuta.innerHTML);
+      localStorage.setItem("namn", namnRuta.innerHTML);
+    });
+};
+
+
+console.log(localStorage);
