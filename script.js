4996dfa6db03f0361f50d85f56f05097d0e1cb51
elin.sjogren <elin.rn.sjogren@gmail.com>
2023-09-28 22:58:20 +0200

    varning om man skriver fel lösenord.

--- script.js
+++ script.js
@@ -17,10 +17,20 @@ window.onload = (event) =>{
 
         localStorage.setItem("losen", losenInmatning);
         localStorage.setItem("namn", namnInmatning);
+        taFram();
       }
-      
+      else{
+        //informera användaren att lösen eller användarnamn är fel
+        let formular = document.getElementById('loggaInFormular');
+        const pElem = document.createElement('p')
+        pElem.innerHTML = "Fel användarnamn eller lösenord. :(";
+        formular.appendChild(pElem);
+      }
     
     });
 };
 
 //funktion för att göra innehåll synligt/osynligt
+function taFram(){
+  document.getElementById('informationSomInloggad').classList.remove('osynlig');
+}
